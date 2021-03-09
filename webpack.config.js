0b9b6b14b5dacbc5fa7fb783f07b3f455aff836d
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-03-09 18:58:59 -0300

    chore: html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
-const path = require("path") //work with node paths
+const path = require('path') //work with node paths
+const HtmlwebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname,"src","index.jsx"),
+  entry: path.resolve(__dirname,'src','index.jsx'),
   output: {
-    path: path.resolve(__dirname,"dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
   },
 
   resolve: {
     extensions: ['.js', '.jsx']
   },
-
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: path.resolve(__dirname,'public','index.html')
+    })
+  ],
   module: {
     /**
      * Rules for read js files
@@ -23,7 +28,7 @@ module.exports = {
         test: /\.jsx$/, // Execute only that .js extensions
         exclude: /node_modules/, //Dont transpile node_modules .js files on babel
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       }
     ]
