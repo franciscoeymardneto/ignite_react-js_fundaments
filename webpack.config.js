5f21a78fc99a888f0e9c0b97b8d5b600bbc79360
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-03-09 20:23:16 -0300

    chore: add node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         }
       },
       {
-       test: /\.css$/,
+       test: /\.scss$/,
        exclude: /node_modules/,
        use:['style-loader', 'css-loader', 'sass-loader'] 
       }
